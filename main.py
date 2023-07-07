14f5f97b343b4c13a184ab6007e2a9f5568bb7c4
helia <heliyash4380>
2023-07-07 09:53:30 +0330

    some changes

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-import networkx as nx
+
+# import networkx as nx
+from minizinc import Instance, Model, Solver
+
+
+# k_means = Model("./Linear Programming/K_means.mzn")
+# gecode = Solver.lookup("gecode")
+# instance = Instance(gecode, k_means)
+
+
+# instance["n"] = {2}
+# instance["k"] = {2}
+# instance["D"] = [[2, 2], [-3 , -3]]
+
+
+# result = instance.solve()
+
+import pymzn
+solns = pymzn.minizinc('./Linear Programming/K_means.mzn', './Linear Programming/data.dzn', data={'capacity': 20})
+print(solns)
+
+print(len(solns))
+print(solns.status)
